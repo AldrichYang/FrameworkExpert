851984ce31198970c8f165d1c65947f5e0315fe7
yanghengx <yanghengx@nonobank.com>
2018-09-03 18:35:34 +0800

    构造函数和测试方法的参数注入

---
+++ src/test/java/methodParamsInjectDemo/TestReporterDemo.java
@@ -0,0 +1,28 @@
+package methodParamsInjectDemo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestReporter;
+
+import java.util.HashMap;
+
+/**
+ * Created by yh on 9/3/18.
+ * 使用TestReporter来代替你在JUnit 4中打印信息到stdout或stderr的习惯。
+ * 使用@RunWith(JUnitPlatform.class)会将报告的所有条目都输出到stdout中
+ */
+public class TestReporterDemo {
+
+    @Test
+    void reportSingleValue(TestReporter testReporter) {
+        testReporter.publishEntry("a key", "a value");
+    }
+
+    @Test
+    void reportSeveralValues(TestReporter testReporter) {
+        HashMap<String, String> values = new HashMap<>();
+        values.put("user name", "dk38");
+        values.put("award year", "1974");
+
+        testReporter.publishEntry(values);
+    }
+}
