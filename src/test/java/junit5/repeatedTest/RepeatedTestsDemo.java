eeb4e740609dbce4abe65f63da6787bcfb92fa82
yanghengx <yanghengx@nonobank.com>
2018-09-04 16:36:46 +0800

    添加方法说明

--- src/test/java/junit5/repeatedTest/RepeatedTestsDemo.java
+++ src/test/java/junit5/repeatedTest/RepeatedTestsDemo.java
@@ -31,18 +31,21 @@ public class RepeatedTestsDemo {
         assertEquals(5, repetitionInfo.getTotalRepetitions());
     }
 
+//    自定义显示名称与自定义模式组合在一起，然后使用TestInfo来验证生成的显示名称的格式
     @RepeatedTest(value = 1, name = "{displayName} {currentRepetition}/{totalRepetitions}")
     @DisplayName("Repeat!")
     void customDisplayName(TestInfo testInfo) {
         assertEquals(testInfo.getDisplayName(), "Repeat! 1/1");
     }
 
+//    上述预定义的RepeatedTest.LONG_DISPLAY_NAME模式
     @RepeatedTest(value = 1, name = RepeatedTest.LONG_DISPLAY_NAME)
     @DisplayName("Details...")
     void customDisplayNameWithLongPattern(TestInfo testInfo) {
         assertEquals(testInfo.getDisplayName(), "Details... :: repetition 1 of 1");
     }
 
+//    演示将重复测试的显示名称翻译成外语的能力
     @RepeatedTest(value = 5, name = "Wiederholung {currentRepetition} von {totalRepetitions}")
     void repeatedTestInGerman() {
         // ...
